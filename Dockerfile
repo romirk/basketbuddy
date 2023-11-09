dea40fb1dbbeaa10d94b338dde15542b1c464f59
romir kulshrestha <romir.kulshrestha@gmail.com>
2023-11-09 15:44:54 +0100

    fix ros build problems

--- Dockerfile
+++ Dockerfile
@@ -3,7 +3,8 @@ FROM ros:humble-ros-core
 RUN apt update && apt upgrade -y && \
     apt install -y git python3 python3-pip \
     libudev-dev ros-${ROS_DISTRO}-slam-toolbox \
-    python3-rospkg \
+    python3-rosdep python3-colcon-common-extensions \
+    ros-${ROS_DISTRO}-diagnostic-updater \
     && apt-get autoremove -y && \
     apt-get clean && \
     rm -rf /var/lib/apt/lists/*
--- scripts/setup.sh
+++ scripts/setup.sh
@@ -9,7 +9,8 @@ git pull
 git checkout $BRANCH
 
 source /opt/ros/humble/setup.bash
-rosdep install --from-paths src --ignore-src -r -y
-colcon build --symlink-install --cmake-args=-DCMAKE_BUILD_TYPE=Release
+rosdep init && rosdep update || exit 1
+rosdep install --from-paths src --ignore-src -r -y || exit 1
+colcon build --symlink-install --cmake-args=-DCMAKE_BUILD_TYPE=Release || exit 1
 
 popd || exit 1 # $BB_ROOT/ros_ws
